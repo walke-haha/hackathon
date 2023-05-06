a76bad066798b4ecbcd4a1486d5771bace4c404a
mkriti04 <127508468+mkriti04@users.noreply.github.com>
2023-05-07 04:05:13 +0530

    Add files via upload

---
+++ x.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+cursor = conn.cursor()
+cursor = conn.cursor()
+cursor.execute('''CREATE TABLE students(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    rollno INTEGER,
+    email TEXT,
+    contact INTEGER,
+    branch TEXT,
+    address TEXT
+);''')
+               
+cursor.execute('''CREATE TABLE faculty (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    faculty_id INTEGER,
+    email TEXT,
+    phone_number INTEGER,
+    research_areas TEXT,
+    designation TEXT,
+    qualifications TEXT
+);''')
+
+#cursor.execute('DELETE FROM songs')
+# cursor.execute('SELECT name FROM songs')
+# r=cursor.fetchall()
+# print(r)
+conn.commit()
+
+conn.close()
