fdb8b0c96e7914a49554b29c6e280754b251b61f
sama <sama.walke@students.iiit.ac.in>
2023-05-07 10:10:24 +0530

    try 3

--- app1.py
+++ app1.py
@@ -30,9 +30,14 @@ def addPage():
 @app.route('/students.html')
 def students():
     return render_template("students.html")
-# @app.route('/SearchPage.html')
-# def searchPageRelay():
-#      return render_template("SearchPage.html")
+
+@app.route('/faculties.html')
+def faculties():
+    return render_template("faculties.html")
+
+@app.route('/home.html')
+def home():
+     return render_template("home.html")
 
 # @app.route('/searchStu.html')
 # def stuSearch():
@@ -65,6 +70,70 @@ def addStudent() -> str:
                 database.commit()
 
                 return render_template("home.html")
+            
+
+@app.route("/faculty", methods = ['POST'])
+def addfaculty() -> str:
+    data = {"name": "",  "faculty_id": "", "email": "", "phone_number": "", "qualifications": ""}
+    name = request.form.get("name")
+    f_id= request.form.get("faculty_id")
+    email = request.form.get("email")
+    contact = request.form.get("phone_number")
+    qualifications = request.form.get("qualifications")  
+    
+    with sqlite3.connect('database.db') as database:
+            cursor = database.cursor()
+
+            query = f"SELECT rollno FROM students WHERE faculty_id = {f_id}"
+            cursor.execute(query)
+            data = cursor.fetchall()
+
+            if len(data) != 0:
+                return render_template("fail.html")
+            else:
+                cursor.execute("INSERT INTO students (name, rollno, email, contact, branch) VALUES (?, ?, ?, ?, ?)", (name, f_id, email, contact, qualifications))
+                database.commit()
+
+                return render_template("home.html")
+            
+
+@app.route('/display.html')
+def display():
+    with sqlite3.connect('database.db') as database:
+        cursor = database.cursor()
+        cursor.execute("SELECT * FROM students")
+        students = cursor.fetchall()
+
+        return render_template("display.html", students=students)
+    
+@app.route("/students/delete", methods=["POST"])
+def deleteStudent():
+    id = request.form.get("id")
+    with sqlite3.connect('database.db') as database:
+        cursor = database.cursor()
+        cursor.execute("DELETE FROM students WHERE id = ?", (id,))
+        database.commit()
+    return redirect(url_for("display"))
+
+@app.route('/display1.html')
+def display1():
+    with sqlite3.connect('database.db') as database:
+        cursor = database.cursor()
+        cursor.execute("SELECT * FROM faculty")
+        s = cursor.fetchall()
+
+        return render_template("display1.html", s=students)
+@app.route("/s/delete", methods=["POST"])
+def deleteStudent1():
+    id = request.form.get("id")
+    with sqlite3.connect('database.db') as database:
+        cursor = database.cursor()
+        cursor.execute("DELETE FROM faculty WHERE id = ?", (id,))
+        database.commit()
+    return redirect(url_for("display"))
+
+
+
 if __name__ == "__main__":
 	app.run(debug = True, port=5000)
     
--- x.py
+++ x.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect('database.db')
 cursor = conn.cursor()
 cursor = conn.cursor()
-cursor.execute('''CREATE TABLE students(
+cursor.execute('''CREATE TABLE IF NOT EXISTS students(
     id INTEGER PRIMARY KEY,
     name TEXT,
     rollno INTEGER,
@@ -12,21 +12,15 @@ cursor.execute('''CREATE TABLE students(
     branch TEXT
 );''')
                
-cursor.execute('''CREATE TABLE faculty (
+cursor.execute('''CREATE TABLE IF NOT EXISTS faculty (
     id INTEGER PRIMARY KEY,
     name TEXT,
     faculty_id INTEGER,
     email TEXT,
     phone_number INTEGER,
-    research_areas TEXT,
-    designation TEXT,
     qualifications TEXT
 );''')
 
-#cursor.execute('DELETE FROM songs')
-# cursor.execute('SELECT name FROM songs')
-# r=cursor.fetchall()
-# print(r)
 conn.commit()
 
 conn.close()
